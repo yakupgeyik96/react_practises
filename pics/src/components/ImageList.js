1e8c8528544c1f4bd7403afcc3d195f76f5a152e
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-07-16 18:20:52 +0300

    15.07.2020

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 
-    const images = props.images.map(({description, id, urls}) => {
+    const images = props.images.map((image) => {
         return (
-            <img alt={description} key={id} src={urls.regular} />
+            <ImageCard key={image.id} image={image} />
         );
     });
 
     return (
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     );
 }
 
