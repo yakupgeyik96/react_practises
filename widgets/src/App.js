cef50d186ece848f95d850c85bdc4a520977f88c
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-07-24 19:24:54 +0300

    24.07.2020, 16:30

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from 'react';
+//import Accordion from './components/Accordion';
+//import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items = [
+/*const items = [
     {
         title: 'What is React?',
         content: 'React is a front end javascript framework'
@@ -15,12 +16,36 @@ const items = [
         title: 'How do you use React',
         content: 'You use React creating components'
     }
+]; */
+
+const options = [
+    {
+        label : 'The Color Red',
+        value: 'red'
+    },
+    {
+        label : 'The Color Green',
+        value : 'green'
+    },
+    {
+        label : 'The Color Blue',
+        value : 'blue'
+    }
 ];
 
 export default () => {
+
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div>
-            <Search />
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ?    
+                <Dropdown selected={selected}
+                onSelectedChange={setSelected}
+                options={options} /> : null
+            }
         </div>
     );
 }
